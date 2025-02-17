ce300f66edfb75f013efdf894453817d9a35e7a4
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-02-17 10:18:52 +0700

    main: Added authentication URL constants

---
+++ src/api/urls/authentication.ts
@@ -0,0 +1,13 @@
+export const AUTH_URL = "/auth";
+
+export const LOGIN_URL = `${AUTH_URL}/login`;
+
+export const REGISTER_URL = `${AUTH_URL}/register`;
+
+export const LOGOUT_URL = `${AUTH_URL}/logout`;
+
+export const REFRESH_URL = `${AUTH_URL}/refresh`;
+
+export const FORGOT_PASSWORD_URL = `${AUTH_URL}/forgot-password`;
+
+export const RESET_PASSWORD_URL = `${AUTH_URL}/reset-password`;
